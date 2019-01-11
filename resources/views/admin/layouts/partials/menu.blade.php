2aca2e5ff8c998d0edbc1c37093277f9edfb035a
mstiven013 <32859798+mstiven013@users.noreply.github.com>
2019-01-10 19:42:48 -0500

    Agregar validaciones de roles en el menú del administrador

--- resources/views/admin/layouts/partials/menu.blade.php
+++ resources/views/admin/layouts/partials/menu.blade.php
@@ -1,97 +1,110 @@
 <ul class="nav-menu" id="nav-menu">
     <ul class="vmenu">
-        <li>
-            <a data-id="books">
-                <span class="icon icon-books"></span> <span class="text">Libros</span>
-            </a>
 
-            <ul class="submenu" data-menu="books">
-                <li><a href="/am-admin/libros">Todos los libros</a></li>
-                <li><a href="/am-admin/libros/create">Añadir libro</a></li>
-                <li><a href="/am-admin/libros/inventario">Inventario</a></li>
-            </ul>
-        </li>
-        <li>
-            <a data-id="specialties">
-                <span class="icon icon-folder-open"></span> <span class="text">Especialidades</span>
-            </a>
+        @if (session('user')->role[0] == 'SUPERADMIN')
+            <li>
+                <a data-id="books">
+                    <span class="icon icon-books"></span> <span class="text">Libros</span>
+                </a>
 
-            <ul class="submenu" data-menu="specialties">
-                <li><a href="/am-admin/especialidades">Todos las especialidades</a></li>
-                <li><a href="/am-admin/especialidades/create">Añadir especialidad</a></li>
-            </ul>
-        </li>
-        <li>
-            <a data-id="authors">
-                <span class="icon icon-profile"></span> <span class="text">Autores</span>
-            </a>
+                <ul class="submenu" data-menu="books">
+                    <li><a href="/am-admin/libros">Todos los libros</a></li>
+                    <li><a href="/am-admin/libros/create">Añadir libro</a></li>
+                    <li><a href="/am-admin/libros/inventario">Inventario</a></li>
+                </ul>
+            </li>
+            <li>
+                <a data-id="specialties">
+                    <span class="icon icon-folder-open"></span> <span class="text">Especialidades</span>
+                </a>
 
-            <ul class="submenu" data-menu="authors">
-                <li><a href="/am-admin/autores">Todos los autores</a></li>
-                <li><a href="/am-admin/autores/create">Añadir autor</a></li>
-            </ul>
-        </li>
-        <li>
-            <a data-id="sliders">
-                <span class="icon icon-images"></span> <span class="text">Sliders</span>
-            </a>
+                <ul class="submenu" data-menu="specialties">
+                    <li><a href="/am-admin/especialidades">Todos las especialidades</a></li>
+                    <li><a href="/am-admin/especialidades/create">Añadir especialidad</a></li>
+                </ul>
+            </li>
+            <li>
+                <a data-id="authors">
+                    <span class="icon icon-profile"></span> <span class="text">Autores</span>
+                </a>
 
-            <ul class="submenu" data-menu="sliders">
-                <li><a href="/am-admin/sliders">Todos los slider</a></li>
-                <li><a>Añadir slider</a></li>
-            </ul>
-        </li>
-        <li>
-            <a data-id="coupons">
-                <span class="icon icon-price-tags"></span> <span class="text">Cupones</span>
-            </a>
+                <ul class="submenu" data-menu="authors">
+                    <li><a href="/am-admin/autores">Todos los autores</a></li>
+                    <li><a href="/am-admin/autores/create">Añadir autor</a></li>
+                </ul>
+            </li>
+            <li>
+                <a data-id="sliders">
+                    <span class="icon icon-images"></span> <span class="text">Sliders</span>
+                </a>
 
-            <ul class="submenu" data-menu="coupons">
-                <li><a>Todos los cupones</a></li>
-                <li><a>Añadir cupón</a></li>
-            </ul>
-        </li>
-        <li>
-            <a data-id="orders">
-                <span class="icon icon-database"></span> <span class="text">Pedidos</span>
-            </a>
+                <ul class="submenu" data-menu="sliders">
+                    <li><a href="/am-admin/sliders">Todos los slider</a></li>
+                    <li><a>Añadir slider</a></li>
+                </ul>
+            </li>
+        @endif
 
-            <ul class="submenu" data-menu="orders">
-                <li><a>Ver pedidos</a></li>
-                <li><a>Facturas</a></li>
-                <li><a>Carritos de compra</a></li>
-            </ul>
-        </li>
-        <li>
-            <a data-id="clients">
-                <span class="icon icon-user-tie"></span> <span class="text">Clientes</span>
-            </a>
+        @if (session('user')->role[0] == 'SUPERADMIN' || session('user')->role[0] == 'ADMIN')
+            <li>
+                <a data-id="coupons">
+                    <span class="icon icon-price-tags"></span> <span class="text">Cupones</span>
+                </a>
 
-            <ul class="submenu" data-menu="clients">
-                <li><a>Ver clientes</a></li>
-                <li><a>Direcciones</a></li>
-            </ul>
-        </li>
-        <li>
-            <a data-id="blog">
-                <span class="icon icon-newspaper"></span> <span class="text">Blog</span>
-            </a>
+                <ul class="submenu" data-menu="coupons">
+                    <li><a>Todos los cupones</a></li>
+                    <li><a>Añadir cupón</a></li>
+                </ul>
+            </li>
+            
+            <li>
+                <a data-id="orders">
+                    <span class="icon icon-database"></span> <span class="text">Pedidos</span>
+                </a>
 
-            <ul class="submenu" data-menu="blog">
-                <li><a>Ver publicaciones</a></li>
-                <li><a>Categorías</a></li>
-            </ul>
-        </li>
-        <li>
-            <a data-id="users">
-                <span class="icon icon-users"></span> <span class="text">Usuarios</span>
-            </a>
+                <ul class="submenu" data-menu="orders">
+                    <li><a>Ver pedidos</a></li>
+                    <li><a>Facturas</a></li>
+                    <li><a>Carritos de compra</a></li>
+                </ul>
+            </li>
 
-            <ul class="submenu" data-menu="users">
-                <li><a href="/am-admin/usuarios">Ver todos los usuarios</a></li>
-                <li><a href="/am-admin/usuarios/create">Añadir usuario</a></li>
-            </ul>
-        </li>
+            <li>
+                <a data-id="clients">
+                    <span class="icon icon-user-tie"></span> <span class="text">Clientes</span>
+                </a>
+
+                <ul class="submenu" data-menu="clients">
+                    <li><a>Ver clientes</a></li>
+                    <li><a>Direcciones</a></li>
+                </ul>
+            </li>
+
+            <li>
+                <a data-id="blog">
+                    <span class="icon icon-newspaper"></span> <span class="text">Blog</span>
+                </a>
+
+                <ul class="submenu" data-menu="blog">
+                    <li><a>Ver publicaciones</a></li>
+                    <li><a>Categorías</a></li>
+                </ul>
+            </li>
+        @endif
+
+        @if (session('user')->role[0] == 'SUPERADMIN')
+            <li>
+                <a data-id="users">
+                    <span class="icon icon-users"></span> <span class="text">Usuarios</span>
+                </a>
+
+                <ul class="submenu" data-menu="users">
+                    <li><a href="/am-admin/usuarios">Ver todos los usuarios</a></li>
+                    <li><a href="/am-admin/usuarios/create">Añadir usuario</a></li>
+                </ul>
+            </li>
+        @endif
+
         <li>
             <a data-id="settings">
                 <span class="icon icon-equalizer"></span> <span class="text">Ajustes</span>
@@ -99,10 +112,15 @@
 
             <ul class="submenu" data-menu="settings">
                 <li><a href="/am-admin/ajustes">Ajustes generales</a></li>
+
+                @if (session('user')->role[0] == 'SUPERADMIN')
+                    <li><a>Plantilla</a></li>
+                @endif
             </ul>
         </li>
+
         <li>
-            <a>
+            <a href="/am-admin/logout">
                 <span class="icon icon-exit"></span> <span class="text">Cerrar sesión</span>
             </a>
         </li>
