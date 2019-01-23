d1ac313a49ad76b8d63bc24a88ce1154a987e7ce
Stiven Lopez <32859798+mstiven013@users.noreply.github.com>
2019-01-23 17:50:26 -0500

    Organizar pagina de busqueda por si no se encuentra ningun libro

--- resources/views/ecommerce/books/search.blade.php
+++ resources/views/ecommerce/books/search.blade.php
@@ -1,6 +1,6 @@
 @extends('ecommerce.layouts.site')
 
-@section('title', "Blog - Amolca Editorial Médica y Odontológica")
+@section('title', "Resultados de busqueda - Amolca Editorial M&eacute;dica y Odontol&oacute;gica")
 
 @section('contentClass', 'page-container search-page')
 @section('content')
@@ -12,7 +12,36 @@
 
 <div class="content-container">
 
-	@include('ecommerce.loops.books.loop', ['books' => $posts, 'type' => 'loop', 'items_per_row' => 4, 'show_links' => 'no'])
+	@if (count($posts) > 0)
+		@include('ecommerce.loops.books.loop', ['books' => $posts, 'type' => 'loop', 'items_per_row' => 4, 'show_links' => 'no'])
+	@else
+		
+		<div class="search-not-found">
+			<p class="title">Lo sentimos pero no existe ningún libro relacionado con su busqueda.</p>
+
+			<ul class="social-list">
+				<li>
+					<a class="button primary" href="/">Ir al Inicio</a>
+				</li>
+				<li>
+					<a target="_blank" href="https://www.facebook.com/EdAmolca/" class="social">
+						<i class="icon-facebook1"></i>
+					</a>
+				</li>
+				<li>
+					<a target="_blank" href="https://www.instagram.com/amolcacolombia/" class="social">
+						<i class="icon-instagram1"></i>
+					</a>
+				</li>
+				<li>
+					<a target="_blank" href="https://twitter.com/EAmolca" class="social">
+						<i class="icon-twitter1"></i>
+					</a>
+				</li>
+			</ul>
+		</div>
+
+	@endif
 
 </div>
 @endsection
