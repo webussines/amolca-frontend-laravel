f0a22368e739c2bf75278b3f83d8dc59c7a3886e
Stiven Lopez <32859798+mstiven013@users.noreply.github.com>
2019-01-30 14:08:39 -0500

    Redirección en el login del admin si ya hay un usuario activo

--- app/Http/Controllers/Admin/AdminHomeController.php
+++ app/Http/Controllers/Admin/AdminHomeController.php
@@ -8,10 +8,18 @@ use App\Http\Controllers\Controller;
 class AdminHomeController extends Controller
 {
     public function index() {
+
+    	if(session('user') && session('user')->role !== 'CLIENT') {
+    		return redirect('/am-admin/libros');
+    	}
+
     	return view('admin.auth.login');
+
     }
 
     public function dashboard() {
+
     	return view('admin.dashboard');
+    	
     }
 }
